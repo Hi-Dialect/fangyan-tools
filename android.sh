ede4a4526afb15f53cd98dcd4cdb4d7b2dbf8a18
shuzhiwen <448875211@qq.com>
2020-03-12 18:23:10 +0900

    local

---
+++ android.sh
@@ -0,0 +1,20 @@
+#!/bin/bash  
+
+echo "选择要执行的命令: "
+echo "1. 移动 h5 内容至 assets 目录下"
+echo "2. 启动 android studio"
+
+read -p "请输入序号: " number
+
+#if条件语句内前后需要空格
+if [ $number -eq 1 ]
+then 
+    sudo cp -r index.html android/app/src/main/assets
+    sudo cp -r frame.css android/app/src/main/assets
+    sudo cp -r video.js android/app/src/main/assets
+elif [ $number -eq 2 ]
+then
+    sh /home/yuwen/AndroidStudio/bin/studio.sh
+else
+    echo "输入有误"
+fi
--- copy.sh
+++
@@ -1,3 +0,0 @@
-sudo cp -r index.html android/app/src/main/assets
-sudo cp -r frame.css android/app/src/main/assets
-sudo cp -r video.js android/app/src/main/assets
