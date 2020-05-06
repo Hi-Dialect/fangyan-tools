fa26ca3fe30a267daeb55d4671af06e6d3061407
shuzhiwen <448875211@qq.com>
2020-05-06 14:50:46 +0800

    local

--- android/app/src/main/assets/tools.js
+++ android/app/src/main/assets/tools.js
@@ -151,9 +151,9 @@ function handlePlay(id) {
     if (id == 'recordingVideo') {
         //更改图标
         if (video.paused) {
-            $('recordingPlay').attr('xlink:href', 'icon-zanting');
+            $('#recordingPlay').attr('xlink:href', '#icon-zanting');
         } else {
-            $('recordingPlay').attr('xlink:href', 'icon-bofang');
+            $('#recordingPlay').attr('xlink:href', '#icon-bofang');
         }
         //后端调用
         if (recordingStart) {
